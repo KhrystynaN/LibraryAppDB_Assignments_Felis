b890849c93aa1ab628c29e6015f9c8204b777914
Suada_Cydeo <demcesuada03@gmail.com>
2023-03-29 10:40:12 -0400

    US09AC1-Total amount of borrowed books module

--- src/test/java/com/library/steps/Hooks.java
+++ src/test/java/com/library/steps/Hooks.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    @Before
+    @Before("@ui")
     public void setUp(){
         System.out.println("this is coming from BEFORE");
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -23,7 +23,7 @@ public class Hooks {
 
     }
 
-    @After
+    @After("@ui")
     public void tearDown(Scenario scenario){
         System.out.println("this is coming from AFTER");
 
@@ -36,17 +36,23 @@ public class Hooks {
 
     }
 
-    @Before
+    @Before("@db")
     public void setUpDB(){
-        System.out.println("DB connection is done");
+        System.out.println("DB connection is DONE ");
+
         DB_Util.createConnection();
+
     }
 
-    @After
+    @After("@db")
     public void destroyDB(){
-        System.out.println("DB connection is close");
+        System.out.println("DB connection is closed ");
+
         DB_Util.destroy();
+
     }
+
+
 
 
 }
