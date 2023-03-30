762c36f4febc2880ad740b1ebe8ac905633d11f0
noor <nooralameri312@gmail.com>
2023-03-30 07:26:31 -0400

    US06 finished and ready to merge!

--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -1,3 +1,26 @@
 select count(id) from users;
 
 select distinct count(id) from users;
+
+
+
+select * FROM books
+where author='Noor';
+
+
+
+
+select * FROM books
+where isbn ='The Scrum Field Guide';
+
+select * FROM books
+where name='Head First Java  ';
+
+
+select id,name,author from books
+where name = 'Head First Java';
+
+
+select id,name,author from books
+where name = 'The Scrum Field Guide' and author='Mitch Lacey '
+order by id desc;
